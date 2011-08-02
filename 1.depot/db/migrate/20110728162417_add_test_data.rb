ae09d7a732f00362027a19060eb9a57892cb1766
Eko Suprapto Wibowo <swdev.bali@gmail.com>
2011-08-02 15:03:17 +0700

    -prettify list of products
    learn : 1. rails has cycle to toggle rows 2. gsub to substitute string
    of html 3. rake db:migrate version = 0 to revert all db, and rake
    db:migrate to restore it back. 4. delete … sort of POS? 5.
    /public/stylesheet and /public/javascript to store those resource 6.
    index.html.erb in /app/views/layout to store the layout of a controller.
    Solution for templating maybe

--- 1.depot/db/migrate/20110728162417_add_test_data.rb
+++ 1.depot/db/migrate/20110728162417_add_test_data.rb
@@ -1,15 +1,23 @@
 class AddTestData < ActiveRecord::Migration
   def self.up
-      Product.delete_all
-      Product.create(
-        :title => 'Pragmatic Version Control',
-        :description => %{<p>
+    Product.delete_all
+    Product.create(
+                   :title => 'Pragmatic Version Control',
+                   :description => %{<p>
                         This book is a recipe-based approach to using Subversion that will get you up and running quickly---and correctly. All projects need version control: it's a foundational piece of any project's infrastructure. Yet half of all project teams in the U.S. don't use any version control at all. Many others don't use it well, and end up experiencing time-consuming problems.
 </p>}, 
-       :image_url => '/images/svn.jpg', 
-       :price => 28.50) #...
+                   :image_url => '/images/svn.jpg', 
+                   :price => 28.50
+                   ) #...
+    Product.create(
+                   :title => 'A Brief History of Time',
+                   :description => 'A path for me',
+                   :image_url => '/images/hawking.jpg',
+                   :price => 50000
+                   )
   end
 
   def self.down
+    Product.delete_all
   end
 end
